a9aa85e1a3689b9fca477604cf52ebfc369d6a52
Subham Nayak <81096401+subhu2008@users.noreply.github.com>
2026-07-22 18:16:25 +0530

    Fix extraction command for toolchain installation

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ rm -rf "${WDIR}/dist" \
 if [ ! -d "${WDIR}/kernel_platform/prebuilts" ]; then
     echo -e "[+] Downloading and installing Toolchain...\n"
     sudo apt install rsync p7zip-full -y
-    curl -LO --progress-bar https://github.com/ravindu644/android_kernel_sm_x810/releases/download/toolchain/qcom-5.15-toolchain.tar.gz.zip
-    curl -LO --progress-bar https://github.com/ravindu644/android_kernel_sm_x810/releases/download/toolchain/qcom-5.15-toolchain.tar.gz.z01
+    curl -LO https://github.com/ravindu644/android_kernel_sm_x810/releases/download/toolchain/qcom-5.15-toolchain.tar.gz.zip
+    curl -LO https://github.com/ravindu644/android_kernel_sm_x810/releases/download/toolchain/qcom-5.15-toolchain.tar.gz.z01
     7z x qcom-5.15-toolchain.tar.gz.zip && rm qcom-5.15-toolchain.tar.gz.zip qcom-5.15-toolchain.tar.gz.z01
-    tar -xvf qcom-5.15-toolchain.tar.gz && rm qcom-5.15-toolchain.tar.gz
+    tar -xf qcom-5.15-toolchain.tar.gz && rm qcom-5.15-toolchain.tar.gz
     mv prebuilts "${WDIR}/kernel_platform" && chmod -R +x "${WDIR}/kernel_platform/prebuilts"    
 fi
 
